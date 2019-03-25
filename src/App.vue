bcc06a6b24c4378f7ee61af19fc1d49e1584652b
Fabián Jaramillo <fabiancoj@hotmail.com>
2019-03-25 17:08:21 -0500

    se crea la conexión del consumo del api de lastfm, y se obtiene y se listan los artistas top de colombia

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>FcMusic</h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li v-for= "artist in artists" :key= "artist.name"> {{ artist.name }} </li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
   </div>
 </template>
 
 <script>
+import getArtist from './api'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      artists:[]
     }
+  },
+  mounted: function (){
+    const _this = this
+    getArtist()
+      .then(function(artists){
+        _this.artists = artists
+      })
   }
 }
 </script>
---
+++ src/api/config.js
@@ -0,0 +1,15 @@
+export default{
+    appName: 'FcMusic',
+    apiKey: 'dd4847353d45bf73cf0e662e86d8d9c4',
+    secret: '8f80b98d0b9604410ddd8e2013acfca6',
+    refisteredto: 'fcjaramillo'
+}
+
+
+/*API account created
+Here are the details of your new API account.
+
+Application name	FcMusic
+API key	dd4847353d45bf73cf0e662e86d8d9c4
+Shared secret	8f80b98d0b9604410ddd8e2013acfca6
+Registered to	fcjaramillo*/
